72e2ff32d6b46c7a2b503f77e7ed9cbbe95f34bd
RC0030127 <RC0030127@student.riversidecollege.ac.uk>
2018-01-24 13:52:11 +0000

    Add project files.

---
+++ forces/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Force = Convert.ToDouble(textBox2.Text);
+            double Angle = Convert.ToDouble(textBox1.Text);
+            double Fx = Force * cos(Angle);
+            double Fy = Force * sin(Angle);
+            MessageBox.Show("Fx =" + Fx);
+            MessageBox.Show("Fy =" + Fy);
+
+
+
+        }
+        double sin (double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.0));
+        }
+
+
+        double cos (double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.0));
+        }
+
+
+    }
+}
